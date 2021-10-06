c50d2cf4ab436fb36ffed5eea7fd453a3078c3f5
atulanand966 <atulanand966@gmail.com>
2021-10-06 15:41:48 +0530

    Adding more logic to the JS file

--- logic.js
+++ logic.js
@@ -1,1 +1,7 @@
 alert("Hello world of JavaScript");
+
+console.log("Logging this to the console");
+const num1 = 2;
+console.log(`Using the ${num1}nd logger
+to log to the console
+by using the template literal`);
